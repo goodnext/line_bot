d94e46ff5b9dd78c703dd0f19d4968374585118a
鷲巣 良次 <washizu@washizuMacBook-Air.local>
2018-09-24 23:41:24 +0900

    push通知メソッド化

--- push.py
+++ push.py
@@ -1,13 +1,15 @@
 import requests
 import json
 
+def line_push(messeage):
 url = 'https://api.line.me/v2/bot/message/push'
+
 data = {
     "to": "Your user ID",
     "messages": [
         {
             "type": "text",
-            "text": "Hello, user!"
+            "text": messeage
         }
     ]
 }
@@ -16,3 +18,12 @@ headers = {
     'Authorization': 'Bearer ' + #アクセストークン#
 }
 requests.post(url, data=json.dumps(data), headers=headers)
+    }
+    requests.post(url, data=json.dumps(data), headers=headers)
+
+def main():
+    #プッシュ通知をする
+    line_push("あいう\nかきく")
+
+if __name__ == "__main__":
+    main()
