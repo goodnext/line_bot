14bafdb55fe9996e1f372db2f1f7d41be27667d7
鷲巣 良次 <washizu@washizuMacBook-Air.local>
2018-09-29 00:27:12 +0900

    献立のリンク先取得

--- push.py
+++ push.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import datetime
 
 def line_push(messeage):
 url = 'https://api.line.me/v2/bot/message/push'
@@ -21,14 +22,26 @@ headers = {
 requests.post(url, data=json.dumps(data), headers=headers)
 
 def exec_scraping():
-    target_url = 'https://www.weekcook.jp/menu/onemenu/dinner/index.html'
+    base_url = 'https://www.weekcook.jp/menu/onemenu/dinner/'
+    target_url = base_url + 'index.html'
     r = requests.get(target_url)         #requestsを使って、webから取得
+    soup = BeautifulSoup(r.text, 'lxml') #html取得
 
-    soup = BeautifulSoup(r.text, 'lxml')
-    print(soup.find_all("a", attrs={"class": "btn btn_17 btn_more"}))
+    #献立のリンクの配列
+    menu_list = []
+    #対象のaタグをループする
+    for a in soup.find_all("a", attrs={"class": "btn btn_17 btn_more"}): 
+        #配列に追加
+        menu_list.append(a.get('href'))
+    return menu_list
 
 def main():
-    exec_scraping()
+    #対象のサイトからリンク先取得
+    # menu_list = exec_scraping()
+    date = datetime.datetime(2018, 1, 1, 0, 0)
+    print(date)
+
+
     #プッシュ通知をする
     # line_push("あいう\nかきく")
 
